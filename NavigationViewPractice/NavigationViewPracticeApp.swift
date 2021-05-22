8aa31803222ff3ab866930566df5fbec4336f2ad
Frederick Javalera <frederickjavalera@gmail.com>
2021-05-22 13:22:15 -0600

    Initial Commit

---
+++ NavigationViewPractice/NavigationViewPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationViewPracticeApp.swift
+//  NavigationViewPractice
+//
+//  Created by Frederick Javalera on 5/22/21.
+//
+
+import SwiftUI
+
+@main
+struct NavigationViewPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
